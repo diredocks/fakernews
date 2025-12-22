ce2d01c38d7f66ef79e19ff5765efb6e5774bb60
dichgrem <brcefy@gmail.com>
2025-12-22 21:24:17 +0800

    feat:start_demo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), VueDevTools()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000", // For Elysia
+        changeOrigin: true,
+      },
+    },
+  },
+});
